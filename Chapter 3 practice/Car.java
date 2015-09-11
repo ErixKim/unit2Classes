dd76b84866d0d0872421cdc14fbafde3b68b5d64
ErixKim <edkim@NN123-HA-06.HS.sd203.org>
2015-09-11 14:17:20 -0500

    Chapter 3
    fda

--- Chapter 3 practice/Car.java
+++ Chapter 3 practice/Car.java
@@ -1,5 +1,4 @@
 
-
 /**
  * A car has a certain fuel efficiency and a certain amount of fuel in the gas tank.
  * The car may be driven which reduces the amount of gas in the fuel tank.
@@ -11,11 +10,9 @@ public class Car
 {
     /** The fuel efficiency for the car measured in units of miles/gallon (mpg) */
     private double fuelEfficiency;
-    
+
     /** The amount of fuel in the tank of the car measured in units of gallons */
     private double fuelInTank;
-    
-    
 
     /**
      * Constructor for objects of class Cart that specifies the fuel efficiency
@@ -35,7 +32,30 @@ public class Car
      */
     public void drive(double distance)
     {
-        
+        fuelInTank -= distance / fuelEfficiency;
+    }
+
+    /**
+     *Returns the number of gallons of gas in the car's tank
+
+     * @return the number of gallons of gas in the car's tank
+     */
+    public double getGasInTank()
+    {
+        return fuelInTank;
+    }
+
+    /**
+     * Increments the amount of gas in the car's tank by the specified number of gallons
+     *
+     * @pre     gallonsOfGas must be positive
+     * @param   gallonsOfGas amount of gas to add to the car's tank
+
+     */
+    public void addGas(double gallonsOfGas)
+    {
+        fuelInTank += gallonsOfGas;
     }
 
 }
+
