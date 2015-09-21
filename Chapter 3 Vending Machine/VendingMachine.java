6436bd5d78c7984983807066f689469b659fc051
ErixKim <edkim@NN123-HA-06.HS.sd203.org>
2015-09-21 14:12:49 -0500

    Vending Machine
    f

---
+++ Chapter 3 Vending Machine/VendingMachine.java
@@ -0,0 +1,57 @@
+
+
+/**
+ * Vending machine that gives soda when a token is inserted. 
+ * 
+ * @author Eric Kim
+ * @version 9/21/15
+ */
+public class VendingMachine
+{
+    /** description of instance variable x (add comment for each instance variable) */
+    private int tokenCount;
+    private int canCount;
+
+    /**
+     * Default constructor for objects of class VendingMachine
+     */
+    public VendingMachine(int token , int can)
+    {
+        this.tokenCount = tokenCount;
+        this.canCount = canCount;
+    }
+
+    /**
+     * Fills up the vending machine with cans
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public void fillUp(int cans)
+    {
+        canCount += cans;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public void insertToken()
+    {
+        tokenCount += 1;
+        canCount -= 1;
+    }
+
+
+}
