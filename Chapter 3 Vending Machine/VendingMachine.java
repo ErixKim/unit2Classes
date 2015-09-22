ca9db414f37b093763186652cd695bf608994251
ErixKim <edkim@NN123-HA-06.HS.sd203.org>
2015-09-22 13:58:41 -0500

    Vending Machine
    fad

--- Chapter 3 Vending Machine/VendingMachine.java
+++ Chapter 3 Vending Machine/VendingMachine.java
@@ -17,8 +17,8 @@ public class VendingMachine
      */
     public VendingMachine(int token , int can)
     {
-        this.tokenCount = tokenCount;
-        this.canCount = canCount;
+        this.tokenCount = token;
+        this.canCount = can;
     }
 
     /**
@@ -35,7 +35,8 @@ public class VendingMachine
     {
         canCount += cans;
     }
-
+
+
     /**
      * An example of a method - replace this comment with your own
      *  that describes the operation of the method
@@ -52,6 +53,37 @@ public class VendingMachine
         tokenCount += 1;
         canCount -= 1;
     }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public int getCanCount()
+    {
+        return canCount;
+    }
 
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public int getTokenCount()
+    {
+        return tokenCount;
+    }
 
 }
