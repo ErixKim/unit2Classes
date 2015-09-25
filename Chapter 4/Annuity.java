d39b71c5a68e4d11ff01ef9723960ee9e48ecf00
ErixKim <edkim@NN123-HA-06.HS.sd203.org>
2015-09-25 14:14:14 -0500

    chapter 4
    fs

---
+++ Chapter 4/Annuity.java
@@ -0,0 +1,14 @@
+public class Annuity
+{
+   public static void main(String[] args)
+   {
+      double i = .08;
+      int pmt = 10000;
+      int n = 20;
+      static double pow(double base, double exponent)
+      
+      double PVann;
+      
+      PVann = PMT(1+i)
+   }
+}
